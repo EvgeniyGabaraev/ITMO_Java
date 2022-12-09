cea37ca53a82705b5eb193a5dd58224f2781e09c
EvgeniyGabaraev <strong.gabaraev@mail.ru>
2022-12-09 13:40:52 +0300

    Исправил замечания в заданиях 2 и 3

--- src/Lesson5/Task2.java
+++ src/Lesson5/Task2.java
@@ -4,21 +4,22 @@ public class Task2 {
     public static void main(String[] args) {
         String string1 = "Потоп";
         System.out.println(string1);
-        String string2 = reverseWord(string1);
-        if (string2.equalsIgnoreCase(string1)){
-            System.out.println("Слово является палиндромом");
-        } else {
-            System.out.println("Слово не является палиндромом");
-        }
+        reverseWord(string1);
+
     }
 
-    public static String reverseWord (String string){
+    public static void reverseWord (String string){
         char[] chararray = string.toCharArray();
         String result = "";
         for (int i = chararray.length-1; i >= 0 ; i--) {
         result = result + chararray[i];
         }
         System.out.println(result);
-        return result;
+
+        if (string.equalsIgnoreCase(result)){
+            System.out.println("Слово является палиндромом");
+        } else {
+            System.out.println("Слово не является палиндромом");
+        }
     }
 }
--- src/Lesson5/Task3.java
+++ src/Lesson5/Task3.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 public class Task3 {
     public static void main(String[] args) {
         String string1 = "бяка Анна бяка купила бяка автомобиль бяка";
-        String string2 = string1.replace("бяка", "[вырезано цензурой]");
+        String string2 = "бяка";
+        String string3 = "[вырезано цензурой]";
         System.out.println(string1);
-        System.out.println(string2);
-        replaceWord(string1);
+
+        replaceWord(string1, string2, string3);
     }
 
-    public static void replaceWord (String string){
-        String[] newArray = string.split(" ");
+    public static void replaceWord (String string1, String string2, String string3){
+        String[] newArray = string1.split(" ");
         String s1 = "";
         for (int i = 0; i < newArray.length; i++) {
-            if (newArray[i].equalsIgnoreCase("бяка")) {
-                s1 = s1 + " " + "[вырезано цензурой]";
+            if (newArray[i].equalsIgnoreCase(string2)) {
+                s1 = s1 + " " + string3;
             } else {
                 s1 = s1 + " " + newArray[i];
             }
